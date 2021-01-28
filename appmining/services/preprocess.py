d54169a5f213a4066146b896f6b5e79a1250cbcf
AnikaTabassum <35559892+AnikaTabassum@users.noreply.github.com>
2021-01-28 21:30:27 +0600

    done with stemming

--- appmining/services/preprocess.py
+++ appmining/services/preprocess.py
@@ -15,37 +15,62 @@ class Parse(HTMLParser):
  
 	#Defining what the methods should output when called by HTMLParser.
 	def handle_starttag(self, tag, attrs):
-		print("Start tag: ", tag)
+		# print("Start tag: ", tag)
 		for a in attrs:
 			print("Attributes of the tag: ", a)
  
 	def handle_data(self, data):
 		mainLines.append(data)
-		print("Here's the data: ", data)
+		# print("Here's the data: ", data)
  
 	def handle_endtag(self, tag):
-		print("End tag: ", tag)
+		strEnd= "This is end tag"
+		# print("End tag: ", tag)
  
  
 testParser = Parse()
 stri="b'From the makers of award-winning Castle Cats, Dungeon Dogs is an idle RPG that allows you to battle, build, collect and craft all from the comfort of your mobile device.<br/><br/>Join our rebels Lyra, Ken and Poppy on their adventures in Lupinia to take down the evil cat king, who is suppressing the canine population and treating them as second-class citizens.<br/><br/>"
 testParser.feed(lines[0])
-print(len(mainLines))
+# print(len(mainLines))
 # filtered_sentence = [w for w in word_tokens if not w in stop_words] 
 stop_words = set(stopwords.words('english')) 
 filtered_sentence = [] 
 withoutTags=[]##############lines without tags  ------ REMOVING TAGS
 for lineNo in mainLines:
 	result = re.sub(r"http\S+", "", lineNo)
-	# result = re.sub(r"www\S+", "", lineNo)
+	result = re.sub(r"www\S+", "", result)
 	result = re.sub(r"\\t+", "", result)
 	# result= re.sub(r'[^\w]', ' ', result)
 	withoutTags.append(result)
-	print(result)
+	# print(result)
 	
+f = open("output.txt", "w")
 for line in withoutTags: 
 	word_tokens = word_tokenize(line)  
 	for w in word_tokens: 
 		if w not in stop_words:  
 			filtered_sentence.append(w) 
+			f.write(w)
+			f.write(" ")
+############ lines without stop words ----- REMOVING STOP WORDS
 print("sen ", filtered_sentence)################ lines without stop words --------REMOVING STOP WORDS
+
+f.close()
+
+
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import word_tokenize 
+
+
+ps = PorterStemmer() 
+  
+# choose some words to be stemmed 
+words = ["program", "programs", "programer", "programing", "programers"] 
+stemmed_words=[]
+for w in filtered_sentence: 
+    # print(w, " : ", ps.stem(w)) 
+    stemmed_words.append(ps.stem(w))
+####################             stemmed WORDS 					#################################
+for val in stemmed_words:
+	print(val)
+
